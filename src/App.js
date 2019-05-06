c0ef9fdb5e915a70d28146d9b42e2e6a095690f1
Joe <joe@astucc.io>
2019-05-06 08:40:15 -0400

    feat: Adjust font sizes and reword context

--- src/App.js
+++ src/App.js
@@ -4,15 +4,12 @@ import GlobalStyles from './GlobalStyles';
 import { font } from './util'
 
 const Sh1 = styled.h1`
-  ${font.size7};
+  ${font.size8};
+  /* letter-spacing: -.1rem; */
+  margin: 4rem 0 3rem;
   text-align: center;
 `
 
-const Sh2 = styled.h2`
-  ${font.size4};
-  text-align: center;
-  padding-bottom: 1rem;
-`
 const Sp = styled.p`
   ${font.size1};
   line-height: 1.9;
@@ -28,7 +25,7 @@ const Sp = styled.p`
 `
 
 const Sfooter = styled.p`
-  ${font.small};
+  ${font.smallest};
   margin-top: 5rem;
   text-align: center;
 `
@@ -40,11 +37,11 @@ function App() {
         <div className="App">
           <Sh1>Create a type scale using Styled Components</Sh1>
 
-          <Sp>Recently I dove in and started learning Styled Components. I like it overall, even though it is 'Y.A.F.T.' in the endless stack of modern web development.  This is a quick demonstration of how I set up a type scale for React and Gatsby projects. Creating a type scale will improve your workflow and prevent inconsistencies in your work flow.</Sp>
-          <Sp>The minimal styling of this example is intentional. I wanted the code to have the bare minimum styling to make the font sizing easy to find. More information can be found in the blog post explaining how I {" "}
+          <Sp>Creating a type scale will improve your workflow and prevent inconsistencies in your work flow. Recently I dove in and started learning Styled Components. Overall I like Styled Components, even though it is 'Y.A.F.T.' in the endless stack of modern web development.  This is a quick demonstration of how I set up a type scale for React and Gatsby projects. </Sp>
+          <Sp>You can read the blog post explaining how I {" "}
             <a href="https://astucc.io" target="_blank" rel="noopener noreferrer">
             set up a type scale using styled components</a>
-            {" "} or you can visit the {" "}<a href="https://github.com/jastuccio/type-scale-styled-components" target="_blank" rel="noopener noreferrer">github repository</a>
+            {" "} or visit the {" "}<a href="https://github.com/jastuccio/type-scale-styled-components" target="_blank" rel="noopener noreferrer">github repository.</a>{" "} The minimal styling of this example is intentional. I wanted keep it simple while  demonstrating the font sizing in this codesandbox.
           </Sp>
           <Sp>
             <a href="mailto:joe@astucc.io?Subject=Your Styled Components type scale tutorial" target="_top">Get in touch</a>{" "}if you have any questions.
